94dff6383720abfce4a3dcbe9f51de7d2937f9d0
Muhammad awais <awais.malik.q@gmail.com>
2019-06-24 23:41:09 +0500

    In this section make components and set home back ground img

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <app-header></app-header>
+    <app-home></app-home>
     <router-outlet></router-outlet>
     <!-- This is footer container start -->
     <app-footer></app-footer>
